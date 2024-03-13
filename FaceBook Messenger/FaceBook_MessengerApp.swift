aab181c3ece882e84335452bc1ff19663ae82af8
sirx2713 <bradx2713@gmail.com>
2024-03-13 12:09:42 +0300

    Initial Commit

---
+++ FaceBook Messenger/FaceBook_MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceBook_MessengerApp.swift
+//  FaceBook Messenger
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct FaceBook_MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
